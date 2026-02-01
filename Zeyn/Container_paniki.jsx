611501606b6a12c34cf227bc88cf9e9fd1bd8689
TTC <Paniki@gmail.com>
2026-02-01 23:04:29 +0800

    Test Device Paniki 2

--- Zeyn/Container_paniki.jsx
+++ Zeyn/Container_paniki.jsx
@@ -3,7 +3,7 @@ function Container_paniki() {
  
   return (
     <div className="container_zeyn" style={{ backgroundColor: "#933e3e" }}>
-      <h1>Contaianer PANIKI</h1>
+      <h1>Contaianer PANIKI2</h1>
     </div>
   );
 }
